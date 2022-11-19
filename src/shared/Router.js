de7c0b6fae567961e4aa49e919867bda1255cb67
kordobby <poordobby@naver.com>
2022-11-19 20:16:46 +0900

    init

---
+++ src/shared/Router.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "@remix-run/router";
+
+// Route 설정 뼈대 잡기
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/*
+        
+        */}
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
