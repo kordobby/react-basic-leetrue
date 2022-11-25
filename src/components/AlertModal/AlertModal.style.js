29ac1a666ca0e08ebe2c64b96bb25aa1a011cfe6
kordobby <poordobby@naver.com>
2022-11-25 12:34:16 +0900

    비밀의방 생성

--- src/components/AlertModal/AlertModal.style.js
+++ src/components/AlertModal/AlertModal.style.js
@@ -1,24 +1,41 @@
 import styled from "styled-components";
 
+export const StWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+`;
+
 export const AlertWrapper = styled.div`
   width: 300px;
-  height: 300px;
-
-  background-color: yellow;
+  height: 150px;
+  border-radius: 10px;
+  background-color: aliceblue;
 `;
 
 export const AlertTitleWrapper = styled.div`
   display: flex;
+  justify-content: center;
+  align-items: center;
   flex-direction: column;
+  padding: 20px 15px 20px 15px;
 `;
 
 export const AlertBtnWrapper = styled.div`
   display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 export const AlertBtnElem = styled.button`
-  width: 50px;
-  height: 100px;
+  width: 80px;
+  height: 40px;
   background-color: ${({ bgColor }) => bgColor};
-  color: white;
+  color: ${({ color }) => color};
+  margin-right: 5px;
+  margin-left: 5px;
+  border-radius: 3px;
 `;
